bc00c7e2a4be943e1f538579afb192ed012cf85e
Mateis <mateis.pro@outlook.fr>
2023-04-22 15:28:18 +0200

    Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
 	return (
-		<h1>toto</h1>
+		<Container className="my-4">
+			<Routes>
+				<Route path="/" element={<h1>Home</h1>} />
+				<Route path="/new" element={<h1>New</h1>} />
+				<Route path="/:id">
+					<Route index element={<h1>Show</h1>} />
+					<Route path="edit" element={<h1>Edit</h1>} />
+				</Route>
+				<Route path="*" element={<h1>404</h1>} />
+			</Routes>
+		</Container>
 	);
 }
 
